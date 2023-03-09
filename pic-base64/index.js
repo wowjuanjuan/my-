3cbe698393e5f92be99e06c7a38fa72c87ac3b1f
“zhuguangyu” <guangyuzhu@starfission.com>
2023-03-09 12:51:28 +0800

    init

---
+++ pic-base64/index.js
@@ -0,0 +1,248 @@
+var obj = {
+  145: {
+    GoodsID: 145,
+    NFTName: "变身夜",
+    NFTText: "每一个你都那么可爱\n每一个我都这般眼花缭乱",
+    Img: "data:image/png;base64",
+  },
+  146: {
+    GoodsID: 146,
+    NFTName: "彩虹大道",
+    NFTText: "没有思想的云飘过\n我们从未这样快乐过\n",
+    Img: "data:image/png;base64",
+  },
+  147: {
+    GoodsID: 147,
+    NFTName: "颠倒众生",
+    NFTText:
+      "顽皮的臣民怎敢如此旋转乾坤\n有什么不敢\n他们睥睨天下\n不过就是一场游戏\n",
+    Img: "data:image/png;base64",
+  },
+  148: {
+    GoodsID: 148,
+    NFTName: "粉红色的回忆",
+    NFTText: "像一场没有花期的开放\n也许在天涯，也许在身旁\n",
+    Img: "data:image/png;base64",
+  },
+  149: {
+    GoodsID: 149,
+    NFTName: "风定幽深",
+    NFTText:
+      "落叶飞旋如流水\n鸟鸣悄悄钻进树心里\n风不会改变世界的动静\n却把大自然的微妙尽收眼底",
+    Img: "data:image/png;base64",
+  },
+  150: {
+    GoodsID: 150,
+    NFTName: "高山流水花好月圆",
+    NFTText: "守着永不褪色的华彩\n把背影留给黑暗",
+    Img: "data:image/png;base64",
+  },
+  151: {
+    GoodsID: 151,
+    NFTName: "攻占地球",
+    NFTText: "万物重塑，初醒重温\n光有了新的方向",
+    Img: "data:image/png;base64",
+  },
+  152: {
+    GoodsID: 152,
+    NFTName: "好了，你可以闭嘴了",
+    NFTText: "不想成为谁\n却又在成为谁的路上奔跑",
+    Img: "data:image/png;base64",
+  },
+  153: {
+    GoodsID: 153,
+    NFTName: "九个太阳",
+    NFTText: "赤轮收起火焰\n我是金乌，你是白驹\n嬉戏在人间",
+    Img: "data:image/png;base64",
+  },
+  154: {
+    GoodsID: 154,
+    NFTName: "撩一池春水",
+    NFTText: "有趣的灵魂迫不及待\n随时随地\n在别人的故事里\n撩自己的梦",
+    Img: "data:image/png;base64",
+  },
+  155: {
+    GoodsID: 155,
+    NFTName: "灵魂十三号战车",
+    NFTText: "前方是激战的人生\n一个未知而无声的远方",
+    Img: "data:image/png;base64",
+  },
+  156: {
+    GoodsID: 156,
+    NFTName: "灵魂像鸟儿一般自由",
+    NFTText: "你驾驭了光年的翅膀\n亦在我心脉中飞翔\n无始无终",
+    Img: "data:image/png;base64",
+  },
+  157: {
+    GoodsID: 157,
+    NFTName: "没有终点的旅程",
+    NFTText: "毛茸茸的风掠过\n它们轻轻唱着\n我是你的，你是自由的",
+    Img: "data:image/png;base64",
+  },
+  158: {
+    GoodsID: 158,
+    NFTName: "每个人都是自己的孤岛",
+    NFTText: "当潮水退去\n你我终将相连",
+    Img: "data:image/png;base64",
+  },
+  159: {
+    GoodsID: 159,
+    NFTName: "拿什么把美好留住",
+    NFTText:
+      "如果灵魂和躯体可以分离\n如果希望与绝望不再撕扯\n这世界会变得干净吗",
+    Img: "data:image/png;base64",
+  },
+  160: {
+    GoodsID: 160,
+    NFTName: "那朵花儿",
+    NFTText:
+      "它，用灵魂叫醒眼睛\n终结瞳孔的荼靡\n在春天老去时\n落，欢喜的泪\n发光的雨",
+    Img: "data:image/png;base64",
+  },
+  161: {
+    GoodsID: 161,
+    NFTName: "破壁",
+    NFTText: "给生命一道灿烂\n送自由去向彼岸",
+    Img: "data:image/png;base64",
+  },
+  162: {
+    GoodsID: 162,
+    NFTName: "吞食天地",
+    NFTText: "在它的呼吸间\n我努力闪烁\n飞向宇宙深处\n成为它的心跳",
+    Img: "data:image/png;base64",
+  },
+  163: {
+    GoodsID: 163,
+    NFTName: "万物静默如迷",
+    NFTText:
+      "如诗夜色如梦\n时间仿佛无言的涟漪\n叩问悬浮千年的石头\n摘一朵流岚深处的玫瑰\n献给没有谜底的未来",
+    Img: "data:image/png;base64",
+  },
+  164: {
+    GoodsID: 164,
+    NFTName: "望了会儿天",
+    NFTText: "不必仰望\n正如我不会俯瞰",
+    Img: "data:image/png;base64",
+  },
+  165: {
+    GoodsID: 165,
+    NFTName: "微尘",
+    NFTText: "你眼里的宇宙\n是我心中的微尘",
+    Img: "data:image/png;base64",
+  },
+  166: {
+    GoodsID: 166,
+    NFTName: "我灵魂的四周天色已暗",
+    NFTText:
+      "我爱杂草，它有墨绿色的挺拔\n我爱天空，它亲吻翠鸟的羽毛\n当我想去拥抱它们的时候\n世界失去了斑斓\n徒留淡粉的剔透\n照不亮四周",
+    Img: "data:image/png;base64",
+  },
+  167: {
+    GoodsID: 167,
+    NFTName: "下雨天",
+    NFTText: "跟过雨，顶着云\n拉着伞下的晴天\n游人间",
+    Img: "data:image/png;base64",
+  },
+  168: {
+    GoodsID: 168,
+    NFTName: "小欢喜",
+    NFTText:
+      "就是在凡境迷路的那天\n我遇见了你\n你的影子拉长了我的思念\n我们拉着手，来到亮处\n从此\n你是我的翅膀\n我是你的手掌",
+    Img: "data:image/png;base64",
+  },
+  169: {
+    GoodsID: 169,
+    NFTName: "星际迷航",
+    NFTText: "迷失的航线\n不归的冒险\n都无法阻挡\n未来非凡的召唤",
+    Img: "data:image/png;base64",
+  },
+  170: {
+    GoodsID: 170,
+    NFTName: "星之子",
+    NFTText: "待你目若朗星\n宇宙因你璀璨",
+    Img: "data:image/png;base64",
+  },
+  171: {
+    GoodsID: 171,
+    NFTName: "夜晚是宇宙年轻的样子",
+    NFTText:
+      "仰望星空，灵魂与夜晚相逢\n宇宙不老的光芒\n汇聚了少年稚气\n与远古深邃\n狩猎者寻找乐园\n涅槃者向光明跋涉",
+    Img: "data:image/png;base64",
+  },
+  172: {
+    GoodsID: 172,
+    NFTName: "夜巡",
+    NFTText:
+      "我掠过的夜晚\n变成了山，沉默着微笑\n我游刃有余穿梭\n擦出黑暗世界的光\n勾勒着黎明",
+    Img: "data:image/png;base64",
+  },
+  173: {
+    GoodsID: 173,
+    NFTName: "一座城池",
+    NFTText:
+      "迷雾日渐散去\n城市出落清晰\n薛定谔的猫\n巴普洛夫的狗\n游走时光缝隙\n填满一池陆离",
+    Img: "data:image/png;base64",
+  },
+  174: {
+    GoodsID: 174,
+    NFTName: "月圆之夜",
+    NFTText:
+      "这是它们第九百九十九回约架\n黑中有白，白里带黑\n非要分出黑白两道\n绝对有“猫”病",
+    Img: "data:image/png;base64",
+  },
+  175: {
+    GoodsID: 175,
+    NFTName: "Moonrise kingdom",
+    NFTText:
+      "带着童年的玻璃糖纸\n带着青涩的面包树\n奔赴那个不用长大的王国\n做自己永远的主角",
+    Img: "data:image/png;base64",
+  },
+  176: {
+    GoodsID: 176,
+    NFTName: "So was red",
+    NFTText:
+      "我去过你的世界\n他们说你云游在外\n我想把满心的思念\n刻在你会路过的地方\n却不知从何说起……",
+    Img: "data:image/png;base64",
+  },
+  177: {
+    GoodsID: 177,
+    NFTName: "The dark side of the moon",
+    NFTText: "他们看清了卑微的真相\n以无声的爆发力\n凝视宇宙的冷酷与丰厚",
+    Img: "data:image/png;base64",
+  },
+};
+var arr = [
+  "img/1变身夜.png",
+  "img/2彩虹大道.png",
+  "img/3颠倒众生.png",
+  "img/4粉红色的回忆.png",
+  "img/5风定幽深.png",
+  "img/6高山流水花好月圆.png",
+  "img/7攻占地球.png",
+  "img/8好了你可以闭嘴了.png",
+  "img/9九个太阳.png",
+  "img/10撩一池春水.png",
+  "img/11灵魂十三号战车.png",
+  "img/12灵魂像鸟儿一般自由.png",
+  "img/13没有终点的旅程.png",
+  "img/14每个人都是自己的孤岛.png",
+  "img/15拿什么把美好留住.png",
+  "img/16那朵花儿.png",
+  "img/17破壁.png",
+  "img/18吞食天地.png",
+  "img/19万物静默如迷.png",
+  "img/20望了会儿天.png",
+  "img/21微尘.png",
+  "img/22我灵魂的四周天色已暗.png",
+  "img/23下雨天.png",
+  "img/24小欢喜.png",
+  "img/25星际航行.png",
+  "img/26星之子.png",
+  "img/27夜晚是宇宙年轻的样子.png",
+  "img/28夜巡.png",
+  "img/29一座城池.png",
+  "img/30月圆之夜.png",
+  "img/31Moonrise Kingdom.png",
+  "img/32so was red.png",
+  "img/33The Dark Side of The Moon.png",
+];
